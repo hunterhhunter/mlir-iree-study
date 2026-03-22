b9ae0be343fb8675aadca7fe7a24a71fcdb6163e
hunterhhunter <slugger613@naver.com>
2026-03-22 14:07:29 +0900

    add DataLoader README.md

---
+++ runtime-env/src/dataloader/__init__.py
@@ -0,0 +1,45 @@
+"""
+DataLoader Package Initialization & Factory
+
+이 모듈은 벤치마크 프레임워크의 다른 컴포넌트(Runtime, Evaluators 등)에게
+DataLoader 클래스들에 대한 손쉬운 접근(단일 진입점 API)을 제공합니다.
+"""
+
+from ..core.model_spec import Model_Spec, Task
+from .base import DataLoader
+from .image_classification_loader import ImageClassificationLoader
+
+def create_dataloader(model_spec: Model_Spec, **kwargs) -> DataLoader:
+    """
+    Factory Method for DataLoader
+    
+    Model_Spec의 Task 종류(IMAGE_CLASSIFICATION 등)를 분석하여
+    해당 Task에 알맞은 구체 로더(Concrete Loader) 객체를 초기화하여 반환합니다.
+    
+    Args:
+        model_spec (Model_Spec): 로드할 모델의 코어 스펙 규격서
+        **kwargs: dataset_path, image_dir, mean, std 등 구체 로더에 필요한 추가 인자
+        
+    Returns:
+        DataLoader: 추상 베이스 클래스를 상속받은 구체 로더 인스턴스
+        
+    Raises:
+        ValueError: 모델의 Task에 알맞은 로더가 구현되어 있지 않을 경우 발생
+    """
+    task = model_spec.task
+    
+    if task == Task.IMAGE_CLASSIFICATION:
+        return ImageClassificationLoader(model_spec, **kwargs)
+    
+    # 추후 NLP, Object Detection, MatMul 등의 Task 로더가 추가되면
+    # elif task == Task.NLP_CLASSIFICATION: 로직을 확장하면 됩니다.
+    
+    else:
+        raise ValueError(f"현재 '{task.name}' Task를 지원하는 DataLoader가 구현되어 있지 않습니다.")
+
+# 인터페이스 명확화를 위해 외부에서 접근할 수 있는 목록을 __all__ 로 제어
+__all__ = [
+    "DataLoader",
+    "ImageClassificationLoader",
+    "create_dataloader"
+]
